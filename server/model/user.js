c9b0e0d0080d303d0c1671cd9c136f67d5d04470
Seong Han <hans@marc-hi.ca>
2018-01-22 15:54:49 -0500

    Hash user password before saving

--- server/model/user.js
+++ server/model/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
@@ -68,6 +69,19 @@ UserSchema.statics.findByToken = function(token){
   });
 };
 
+UserSchema.pre('save',function (next) {
+  var user = this;
+  if(user.isModified('password')){
+    var hashPw = bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  }else{
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
